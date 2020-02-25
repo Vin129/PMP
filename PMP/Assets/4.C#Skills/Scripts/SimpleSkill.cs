9d803bcadba6c66082adfbf0682ce30ab8589144
vin129 <515019721@qq.com>
2020-02-25 17:14:11 +0800

    add C#skills

---
+++ PMP/Assets/4.C#Skills/Scripts/SimpleSkill.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleSkill{
+	
+	public SimpleSkill(){}
+	
+	public void Skill1()
+	{
+		// int i = null; 
+		int? i = null;
+	}
+
+	struct Skill2Struct
+	{
+		public int a;
+		public int b;
+
+		public void Print(){
+
+		}
+	}
+
+	public void Skill2(){
+		var st = new Skill2Struct();
+		st.a = 1;
+		st.b = 2;
+	}
+}
