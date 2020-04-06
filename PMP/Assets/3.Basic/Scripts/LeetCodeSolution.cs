5922d4934f6664de3f468a98c08f84a1e31e8acf
vin129 <515019721@qq.com>
2020-04-06 20:02:34 +0800

    clock

--- PMP/Assets/3.Basic/Scripts/LeetCodeSolution.cs
+++ PMP/Assets/3.Basic/Scripts/LeetCodeSolution.cs
@@ -67,7 +67,99 @@ public class LeetCodeSolution
         return num;
     }
 	#endregion
+
+	#region 1389
+	///<summary>
+	///1389
+	///给你两个整数数组 nums 和 index。你需要按照以下规则创建目标数组：
+	/// 目标数组 target 最初为空。
+	/// 按从左到右的顺序依次读取 nums[i] 和 index[i]，在 target 数组中的下标 index[i] 处插入值 nums[i] 。
+	/// 重复上一步，直到在 nums 和 index 中都没有要读取的元素。
+	///</summary>
+	public int[] CreateTargetArray(int[] nums, int[] index) {
+        var list = new List<int>();
+        for(int i = 0;i<nums.Length;i++)
+        {
+			list.Insert(index[i],nums[i]);
+        }
+		return list.ToArray();
+    }
+
+	#endregion
+
+	#region 189. 旋转数组
+	///<summary>
+	///189
+	///给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+	///尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+	///要求使用空间复杂度为 O(1) 的 原地 算法。
+	///</summary>
+    public void Rotate1(int[] nums, int k) 
+	{
+		//巧妙使用Array.Reverse 翻转数组的方法
+		//时间复杂度：O(n) 空间复杂度：O(1)
+		k %= nums.Length;
+		if(nums.Length < 2 || k<= 0)
+			return;
+		Array.Reverse(nums,0,nums.Length);
+		Array.Reverse(nums,0,k);
+		Array.Reverse(nums,k,nums.Length - k);
+    }
+
+	public void Rotate2(int[] nums, int k) 
+	{
+		//旋转几次？那就旋转几次吧
+		//时间复杂度：O(kn) 空间复杂度：O(1)
+		k %= nums.Length;
+		if(nums.Length < 2)
+			return;
+		var temp = nums[nums.Length - 1];
+		for(int i = 0;i<k;i++)
+		{
+			temp = nums[nums.Length - 1];
+			for(int n = nums.Length-1;n>0;n--)
+			{
+				nums[n] = nums[n-1];
+			}
+			nums[0] = temp;
+		}
+    }
+
+	public void Rotate3(int[] nums, int k) 
+	{
+		//环状替换
+		//时间复杂度：O(n) 空间复杂度：O(1)
+		k %= nums.Length;
+		if(nums.Length < 2 || k<= 0)
+			return;
+		var swopCount = 0; 
+		for(int i = 0;swopCount<nums.Length;i++)
+		{
+			var cur = i;
+			var temp = nums[i];
+			do
+			{
+				var next = (cur + k) % nums.Length;
+				var t = nums[next];
+				nums[next] = temp;
+				temp = t;
+				cur = next;
+				swopCount++;
+			} 
+			while (cur != i);
+		}
+    }
+
+	public void DoRotate3()
+	{
+
+	}
+
+
+	#endregion
+
 }
+
 
 public static class SolutionExtension
 {
