23b0a0130e99687853fc8f617d1cfcfd272580f8
vin129 <515019721@qq.com>
2020-06-14 18:55:54 +0800

    clock

--- PMP/Assets/4.C#Skills/Scripts/SkillStart.cs
+++ PMP/Assets/4.C#Skills/Scripts/SkillStart.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SkillStart : MonoBehaviour {
 	SimpleSkill mSimpleSkill;
+	private void Awake() {
+		Debug.Log("Awake");
+	}
+
+	private void OnEnable() {
+		Debug.Log("OnEnable");
+	}
+
+	private void Reset() {
+		Debug.Log("Rest");
+	}
+
 	void Start () {
+		Debug.Log("Start");
 		// mSimpleSkill = new SimpleSkill();
 		// mSimpleSkill.Skill11();
 
 		// var s = new Skill13Class();
 		// s.Hellow();
 
-		var s = new SKill14Class();
-		s.DoThread();
+		// var s = new SKill14Class();
+		// s.DoThread();
 	}
 	void Update () {
 		
